2293966abb2eb4f7053ea399ac30f928bf044d70
keith <keith_127@126.com>
2017-02-14 15:14:56 +0800

    Update KTJTextKitTouchHandler.m

--- KTJTextKitTouchHandler.m
+++ KTJTextKitTouchHandler.m
@@ -1,31 +1,5 @@
 //
 //  KTJTextKitTouchHandler.m
-//  
-//
-//  Created by 孙继刚 on 16/5/31.
-//  Copyright © 2016年 madordie. All rights reserved.
-//
-
-#import "KTJTextKitTouchHandler.h"
-
-
-@interface KTJTextKitTouchHandler ()
-
-@property (nonatomic, strong) NSLayoutManager *layoutManager;
-
-@property (nonatomic, strong) NSTextContainer *textContainer;
-
-@property (nonatomic, strong) NSTextStorage *textStorage;
-
-@property (nonatomic, weak) UITextView *textView;
-
-@end
-
-
-@implementation KTJTextKitTouchHandler
-
-//
-//  KTJTextKitTouchHandler.m
 //
 //
 //  Created by 孙继刚 on 16/5/31.
@@ -119,77 +93,6 @@
     }];
 
     return hitItem;
-}
-
-- (NSLayoutManager *)layoutManager
-{
-    if (_layoutManager == nil) {
-        return self.textView.layoutManager;
-    }
-    return nil;
-}
-- (NSTextStorage *)textStorage
-{
-    if (_textStorage == nil) {
-        return self.textView.textStorage;
-    }
-    return nil;
-}
-- (NSTextContainer *)textContainer
-{
-    if (_textContainer == nil) {
-        return self.textView.textContainer;
-    }
-    return nil;
-}
-
-@end
-
-
-@implementation KTJTextKitTouchItem
-
-- (NSString *)attributeName
-{
-    if (_attributeName == nil) {
-        return KTJTextKitTouchHandlerAttributeName;
-    }
-    return _attributeName;
-}
-
-+ (instancetype)itemWithAttributeName:(NSString *)attName value:(id)value rang:(NSRange)rang
-{
-    KTJTextKitTouchItem *item = [KTJTextKitTouchItem new];
-    item.attributeName = attName;
-    item.value = value;
-    item.range = rang;
-    return item;
-}
-
-@end
-
-
-NSString *const KTJTextKitTouchHandlerAttributeName = @"KTJTextKitTouchHandlerAttributeName";
-
-                    if ([obj.attributeName isEqualToString:KTJTextKitTouchHandlerAttributeName]
-                        //                                && obj.value == value
-                        && NSEqualRanges(range, obj.range)) {
-                        hitItem = obj;
-                        *stop = YES;
-                    }
-                }];
-                if (hitItem != nil) {
-                    *stop = YES;
-                }
-            }
-        }
-            }];
-            if (hitItem != nil) {
-        *stop = YES;
-            }
-}
-}];
-
-return hitItem;
 }
 
 - (NSLayoutManager *)layoutManager
