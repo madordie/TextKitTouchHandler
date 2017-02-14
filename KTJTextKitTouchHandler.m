cfa5ac223266517f01a47e432f51d71a2f0c3271
keith <keith_127@126.com>
2017-02-14 15:12:17 +0800

    Update KTJTextKitTouchHandler.m

--- KTJTextKitTouchHandler.m
+++ KTJTextKitTouchHandler.m
@@ -24,6 +24,32 @@
 
 @implementation KTJTextKitTouchHandler
 
+//
+//  KTJTextKitTouchHandler.m
+//
+//
+//  Created by 孙继刚 on 16/5/31.
+//  Copyright © 2016年 madordie. All rights reserved.
+//
+
+#import "KTJTextKitTouchHandler.h"
+
+
+@interface KTJTextKitTouchHandler ()
+
+@property (nonatomic, strong) NSLayoutManager *layoutManager;
+
+@property (nonatomic, strong) NSTextContainer *textContainer;
+
+@property (nonatomic, strong) NSTextStorage *textStorage;
+
+@property (nonatomic, weak) UITextView *textView;
+
+@end
+
+
+@implementation KTJTextKitTouchHandler
+
 - (instancetype)initWithTextView:(UITextView *)view
 {
     self = [super init];
@@ -58,21 +84,92 @@
     __block KTJTextKitTouchItem *hitItem = nil;
 
     [self.textStorage enumerateAttribute:KTJTextKitTouchHandlerAttributeName inRange:NSMakeRange(0, self.textStorage.length) options:0 usingBlock:^(id  _Nullable value, NSRange range, BOOL * _Nonnull stop) {
-//        if (value)
+        //        if (value)
         {
             NSRange glyphRange = [self.layoutManager glyphRangeForCharacterRange:range actualCharacterRange:nil];
-            
+
             [self.layoutManager enumerateEnclosingRectsForGlyphRange:glyphRange withinSelectedGlyphRange:NSMakeRange(NSNotFound, 0) inTextContainer:self.textContainer usingBlock:^(CGRect rect, BOOL * _Nonnull stop) {
 
-        rect.origin.x += self.textView.textContainerInset.left;
-        rect.origin.y += self.textView.textContainerInset.top;
-        rect = UIEdgeInsetsInsetRect(rect, UIEdgeInsetsMake(-2, -2, -2, -2));
+                rect.origin.x += self.textView.textContainerInset.left;
+                rect.origin.y += self.textView.textContainerInset.top;
+                rect = UIEdgeInsetsInsetRect(rect, UIEdgeInsetsMake(-2, -2, -2, -2));
 
-        if (CGRectContainsPoint(rect, location)) {
-            //                    id value = [self.textStorage attribute:KTJTextKitTouchHandlerAttributeName atIndex:range.location effectiveRange:nil];
-            //                    if (value)
-            {
-                [self.touchItems enumerateObjectsUsingBlock:^(KTJTextKitTouchItem *_Nonnull obj, NSUInteger idx, BOOL *_Nonnull stop) {
+                if (CGRectContainsPoint(rect, location)) {
+                    //                    id value = [self.textStorage attribute:KTJTextKitTouchHandlerAttributeName atIndex:range.location effectiveRange:nil];
+                    //                    if (value)
+                    {
+                        [self.touchItems enumerateObjectsUsingBlock:^(KTJTextKitTouchItem *_Nonnull obj, NSUInteger idx, BOOL *_Nonnull stop) {
+                            if ([obj.attributeName isEqualToString:KTJTextKitTouchHandlerAttributeName]
+                                //                                && obj.value == value
+                                && NSEqualRanges(range, obj.range)) {
+                                hitItem = obj;
+                                *stop = YES;
+                            }
+                        }];
+                        if (hitItem != nil) {
+                            *stop = YES;
+                        }
+                    }
+                }
+            }];
+            if (hitItem != nil) {
+                *stop = YES;
+            }
+        }
+    }];
+
+    return hitItem;
+}
+
+- (NSLayoutManager *)layoutManager
+{
+    if (_layoutManager == nil) {
+        return self.textView.layoutManager;
+    }
+    return nil;
+}
+- (NSTextStorage *)textStorage
+{
+    if (_textStorage == nil) {
+        return self.textView.textStorage;
+    }
+    return nil;
+}
+- (NSTextContainer *)textContainer
+{
+    if (_textContainer == nil) {
+        return self.textView.textContainer;
+    }
+    return nil;
+}
+
+@end
+
+
+@implementation KTJTextKitTouchItem
+
+- (NSString *)attributeName
+{
+    if (_attributeName == nil) {
+        return KTJTextKitTouchHandlerAttributeName;
+    }
+    return _attributeName;
+}
+
++ (instancetype)itemWithAttributeName:(NSString *)attName value:(id)value rang:(NSRange)rang
+{
+    KTJTextKitTouchItem *item = [KTJTextKitTouchItem new];
+    item.attributeName = attName;
+    item.value = value;
+    item.range = rang;
+    return item;
+}
+
+@end
+
+
+NSString *const KTJTextKitTouchHandlerAttributeName = @"KTJTextKitTouchHandlerAttributeName";
+
                     if ([obj.attributeName isEqualToString:KTJTextKitTouchHandlerAttributeName]
                         //                                && obj.value == value
                         && NSEqualRanges(range, obj.range)) {
